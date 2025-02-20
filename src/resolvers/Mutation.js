644af64f2c50b5c7ccfe80dad3726da09f473bec
Irvin-Silva <irvin.aurelio27@gmail.com>
2025-02-20 16:06:11 -0500

    Add user and author mutation resolvers with corresponding schema updates

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,6 +1,8 @@
 import { v4 as uuidv4 } from 'uuid';
 
 const Mutation = {
+
+    // Mutation to create user
     createUser: (parent, args, { db }, info) => {
        const isEmailTaken = db.users.some(user => user.email === args.email);
     if(isEmailTaken){
@@ -14,6 +16,7 @@ const Mutation = {
     return user;
     },
 
+    // Mutation to update user
     UpdateUser: (parent, args, {db}, info) => {
         const { id, ...data } = args;
         const userExist = db.users.find(user => user.id === id);
@@ -36,7 +39,39 @@ const Mutation = {
             return user;
         });
         return {...userExist, ...data};
-    }
+    },
+    
+    // Mutation to create author
+    CreateAuthor: (parent, args, { db }, info) => {
+
+        const author = {
+            id: uuidv4(),
+            ...args
+        }
+        db.authors.push(author);
+        return author;
+    },
+
+    // Mutation to update author
+    updateAuthor: (parent, args, { db }, info) => {
+        const { id, ...data } = args;
+        const authorExist = db.authors.find(author => author.id === id);
+
+        if (!authorExist) {
+            throw new Error('Author not found');
+        }
+
+        db.authors = db.authors.map(author => {
+            if(author.id === id){
+                return {
+                    ...author,
+                    ...data
+                }
+            }
+            return author;
+        });
+        return {...authorExist, ...data};
+    },
 
 }
 export default Mutation;
